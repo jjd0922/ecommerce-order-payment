3512ac40d7942a6fbad191ebb4b628320029f6cd
jd <jd0922@naver.com>
2026-05-03 21:50:32 +0900

    애플리케이션: 주문 생성에서 재고 처리를 분리

--- order-application/src/main/java/com/orderpayment/application/order/port/out/InventoryCommandPort.java
+++
@@ -1,8 +0,0 @@
-package com.orderpayment.application.order.port.out;
-
-import com.orderpayment.domain.inventory.Inventory;
-
-public interface InventoryCommandPort {
-
-    void saveInventory(Inventory inventory);
-}
--- order-application/src/main/java/com/orderpayment/application/order/port/out/InventoryQueryPort.java
+++
@@ -1,9 +0,0 @@
-package com.orderpayment.application.order.port.out;
-
-import com.orderpayment.domain.inventory.Inventory;
-import com.orderpayment.domain.product.ProductId;
-
-public interface InventoryQueryPort {
-
-    Inventory getInventory(ProductId productId);
-}
