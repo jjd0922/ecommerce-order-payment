9dcf10c8c17aa5b1431eabb0c9a22a01fd41d794
jd <jd0922@naver.com>
2026-05-03 22:49:25 +0900

    설정: 인프라 JPA 스캔 범위 지정

--- order-api/src/main/java/com/orderpayment/api/OrderPaymentApplication.java
+++ order-api/src/main/java/com/orderpayment/api/OrderPaymentApplication.java
@@ -2,8 +2,12 @@ package com.orderpayment.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication(scanBasePackages = "com.orderpayment")
+@EntityScan(basePackages = "com.orderpayment.infrastructure")
+@EnableJpaRepositories(basePackages = "com.orderpayment.infrastructure")
 public class OrderPaymentApplication {
 
     public static void main(String[] args) {
