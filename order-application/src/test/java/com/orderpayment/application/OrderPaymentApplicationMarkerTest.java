db6caddbb3356233898c86fef4bb3f55fb51fe17
jd <jd0922@naver.com>
2026-05-10 22:04:27 +0900

    테스트: 애플리케이션 계층 테스트 양식 정리

--- order-application/src/test/java/com/orderpayment/application/OrderPaymentApplicationMarkerTest.java
+++ order-application/src/test/java/com/orderpayment/application/OrderPaymentApplicationMarkerTest.java
@@ -1,13 +1,15 @@
 package com.orderpayment.application;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.assertj.core.api.Assertions.assertThat;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class OrderPaymentApplicationMarkerTest {
 
     @Test
-    void markerTypeExists() {
-        assertTrue(OrderPaymentApplicationMarker.class.isInterface());
+    @DisplayName("OrderPaymentApplicationMarker 는 인터페이스이다")
+    void markerType_whenLoaded_thenIsInterface() {
+        assertThat(OrderPaymentApplicationMarker.class.isInterface()).isTrue();
     }
 }
