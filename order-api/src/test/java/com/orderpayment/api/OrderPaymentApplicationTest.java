51820640d27675b2dcffbac5be523dadd3c4806e
jd <jd0922@naver.com>
2026-05-10 22:04:32 +0900

    테스트: API 계층 테스트 양식 정리

--- order-api/src/test/java/com/orderpayment/api/OrderPaymentApplicationTest.java
+++ order-api/src/test/java/com/orderpayment/api/OrderPaymentApplicationTest.java
@@ -1,13 +1,16 @@
 package com.orderpayment.api;
 
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.assertj.core.api.Assertions.assertThatCode;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class OrderPaymentApplicationTest {
 
     @Test
-    void applicationClassExists() {
-        assertDoesNotThrow(() -> OrderPaymentApplication.class.getDeclaredConstructor());
+    @DisplayName("OrderPaymentApplication 클래스는 기본 생성자를 조회할 수 있다")
+    void applicationClass_whenLoaded_thenConstructorIsAvailable() {
+        assertThatCode(() -> OrderPaymentApplication.class.getDeclaredConstructor())
+                .doesNotThrowAnyException();
     }
 }
